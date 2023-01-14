4bafa01a1c743064f53c869c79495cb5d89888e4
Nishchal <nishchal98765@gmail.com>
2023-01-14 16:36:44 +0530

    Algo visualizer

--- audio/script.js
+++
@@ -1,8 +0,0 @@
-import BeatBeat from "./beat-beat.js"
-
-const fn = async() => {
-    const sound = new BeatBeat(new AudioContext(), "./song.mp3")
-    await sound.load()
-    sound.play(() => console.log("This callback will execute at every beat of the song"))
-}
-fn()
